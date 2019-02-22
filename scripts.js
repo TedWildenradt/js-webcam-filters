126d326721189425aa051c7988a46328d474bb0b
TedWildenradt <tedkwildenradt@gmail.com>
2019-02-21 16:12:16 -0800

    Setup webcam functionality

--- scripts.js
+++ scripts.js
@@ -8,5 +8,11 @@ function getVideo(){
   navigator.mediaDevices.getUserMedia({video: true, audio: false})
   .then(localMediaStream => {
     console.log(localMediaStream)
+    video.src = window.URL.createObjectURL(localMediaStream);
+    video.play()
+  })
+  .catch(err => {
+    console.error('shiiiiiit', err)
   })
 }
+getVideo()
