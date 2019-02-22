a442997cbd92be9ead90bd1fd926caf647ba1e36
TedWildenradt <tedkwildenradt@gmail.com>
2019-02-21 16:44:05 -0800

    Refactor code

--- scripts.js
+++ scripts.js
@@ -26,7 +26,8 @@ function paintToCanvas(){
     ctx.drawImage(video, 0, 0, width, height);
     let pixels = ctx.getImageData(0,0,width,height);
     // pixels = redEffect(pixels)
-    pixels = rgbSplit(pixels)
+    // pixels = rgbSplit(pixels)
+    pixels = greenScreen(pixels)
     ctx.globalAlpha = 0.1;
     ctx.putImageData(pixels,0,0);
   }, 16);
